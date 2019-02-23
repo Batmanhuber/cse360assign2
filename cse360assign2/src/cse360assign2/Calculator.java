e22e73e691e272df8b32c905e0ec5575b289df7b
Batmanhuber <mckay.huber@asu.edu>
2019-02-23 12:25:49 -0700

    Starting the assignment

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,33 @@
+package cse360assign2;
+
+public class Calculator {
+	private int total;
+		
+		public Calculator () {
+			total = 0;  // not needed - included for clarity
+		}
+		
+		public int getTotal () {
+			return 0;
+		}
+		
+		public void add (int value) {
+			
+		}
+		
+		public void subtract (int value) {
+			
+		}
+		
+		public void multiply (int value) {
+			
+		}
+		
+		public void divide (int value) {
+			
+		}
+		
+		public String getHistory () {
+			return "";
+		}
+}
---
+++ cse360assign2/src/cse360assign2/Main.java
@@ -0,0 +1,10 @@
+package cse360assign2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
