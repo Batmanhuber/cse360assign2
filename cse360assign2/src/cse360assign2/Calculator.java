af666731b0c74dd1c83f3be111e37e26a5624bdf
Batmanhuber <mckay.huber@asu.edu>
2019-02-23 12:39:53 -0700

    Assignment part 1, adding comments

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,32 +1,46 @@
+/*
+ * By: McKay Huber
+ * Class ID: 408
+ * CSE360 Assignment 2
+ */
+
 package cse360assign2;
 
+//Class Calculator performs the basic operations and can give calculation history
 public class Calculator {
 	private int total;
 		
+		// constructor
 		public Calculator () {
 			total = 0;  // not needed - included for clarity
 		}
 		
+		// returns final value after calculation
 		public int getTotal () {
 			return 0;
 		}
 		
+		// adds and integer to total value
 		public void add (int value) {
 			
 		}
 		
+		// subtracts and integer from total value
 		public void subtract (int value) {
 			
 		}
 		
+		// multiplies the total by an integer
 		public void multiply (int value) {
 			
 		}
 		
+		// divides the total by an integer
 		public void divide (int value) {
 			
 		}
 		
+		// gives the history of calculations in the form of a string
 		public String getHistory () {
 			return "";
 		}
